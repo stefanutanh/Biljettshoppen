a0ac80fe733a96051ed56f1195f2dd0d67e5d3a2
stefanutanh <bergs83@gmail.com>
2024-10-22 06:56:00 +0200

    Add files via upload
    GUI med *.form så man kan se och ändra layouten direkt i intellij

--- src/testbiljettGUI.java
+++ src/testbiljettGUI.java
@@ -1,79 +1,29 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class testbiljettGUI extends JFrame {
-    private JButton button1;
-    private JButton button2;
-    private JComboBox<String> comboBox1;
-    private JComboBox<String> comboBox2;
-    private JPanel mainPanel;
-
+public class testbiljettGUI {
 
     public static void main(String[] args) {
-        // Kör programmet på Event Dispatch Thread för säker GUI-uppdatering
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new testbiljettGUI().setVisible(true);
-            }
-        });
-    } // END MAIN
+        JFrame frameObj = new JFrame("Biljettbetalning");
+        frameObj.setContentPane(new testbiljettGUI().mainPanel);
+        frameObj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameObj.setSize(600,400);
+        frameObj.setVisible(true);
+    }
 
+    private JPanel mainPanel;
+    private JCheckBox hus1CheckBox;
+    private JCheckBox a1000CheckBox;
+    private JCheckBox a1800CheckBox;
+    private JCheckBox hus2CheckBox;
+    private JCheckBox a1500CheckBox;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JCheckBox kortCheckBox;
+    private JCheckBox fakturaCheckBox;
+    private JButton beställButton;
 
     public testbiljettGUI() {
-        // Ställ in JFrame-egenskaper
-        setTitle("Biljettshop");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 200);
 
-        // Skapa panelen
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(3, 1, 10, 10)); // 3 rader, 1 kolumn, 10 px mellanrum
-
-        button1 = new JButton("Val 1");
-        button2 = new JButton("Val 2");
-        comboBox1 = new JComboBox<>(new String[]{"Event 1", "Event 2", "Event 3"});
-        comboBox2 = new JComboBox<>(new String[]{"10:00", "15:00", "18:00"});
-
-        //action listeners för knapparna
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(mainPanel, "Du valde Val 1");
-            }
-        });
-
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(mainPanel, "Du valde Val 2");
-            }
-        });
-
-        comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String selectedEvent = (String) comboBox1.getSelectedItem();
-                JOptionPane.showMessageDialog(mainPanel, "Du valde " + selectedEvent);
-            }
-        });
-        comboBox2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String selectedEvent = (String) comboBox1.getSelectedItem();
-                JOptionPane.showMessageDialog(mainPanel, "Du valde " + selectedEvent);
-            }
-        });
-
-        // Lägg till komponenterna till panelen
-        mainPanel.add(button1);
-        mainPanel.add(button2);
-        mainPanel.add(comboBox1);
-        mainPanel.add(comboBox2);
-
-        // Lägg till panelen till JFrame
-        add(mainPanel);
-    } // END CONSTRUCTOR
-} // END CLASS
+    }
+}
