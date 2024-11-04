4036e6b67c067a6c4c3a758a204675617229c399
William <118085386+WillDBill@users.noreply.github.com>
2024-11-04 13:50:08 +0100

    Update EventBookingApplication.java
    Hardcoded pre-events

--- src/EventBookingApplication.java
+++ src/EventBookingApplication.java
@@ -3,6 +3,14 @@ public class EventBookingApplication {
         // Initialize the service with direct payment as default
         EventBookingService bookingService = new EventBookingService(new DirectPayment());
 
+        
+        Event konsert = new Event(1, "Rock Konsert", "WOOD", 500, 24.99);
+
+        Event forelasning = new Event(2, "RIP Föreläsning", "BRICK", 50, 29.99);
+
+        bookingService.addEvent(konsert);
+        bookingService.addEvent(forelasning);
+
         // Create admin with password "admin123"
         Admin admin = new Admin("admin123");
 
